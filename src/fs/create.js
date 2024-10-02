6cf6b137987c54d9240a221b37b7f9c3b889622c
anturchin <tur4intwo@gmail.com>
2024-10-02 23:08:41 +0300

    feat: implement copy.js and update create.js and utils

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,17 @@
-import path from 'node:path';
 import fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
 
-import { fileExists } from '../utils/index.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const filePath = path.join('files', 'fresh.txt');
+import { exists } from '../utils/index.js';
+
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
 const content = 'I am fresh and young';
 
 const create = async () => {
-    const exists = await fileExists(filePath);
-    if(exists) {
-      throw new Error(`FS operation failed`);
-    }
+    if (await exists(filePath)) throw new Error(`FS operation failed`);
     await fs.writeFile(filePath, content);
 };
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,6 @@ import { access, constants } from 'node:fs/promises';
 
 const toBool = [() => true, () => false];
 
-export const fileExists = (path) => {
+export const exists = (path) => {
     return access(path, constants.R_OK | constants.W_OK).then(...toBool);
 }
