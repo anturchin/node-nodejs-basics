66f8c426e7ff6eb7926462555e3bfb62e2bacfbe
tar <tur4intwo@gmail.com>
2024-10-04 15:12:18 +0300

    feat: implement read.js write.js transform.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import path, { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+
+    const writeStream = createWriteStream(filePath);
+
+    process.stdin.on('data', (chunk) => {
+        const contents = chunk.toString().trim();
+        writeStream.write(contents + '\n');
+    })
+
 };
 
 await write();
