580fdc3171b51cd47b34e245ded0b43281c07500
tar <tur4intwo@gmail.com>
2024-10-04 10:41:34 +0300

    feat: update args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,13 +1,14 @@
 const parseArgs = () => {
 
     const args = process.argv.slice(2);
+    const result = [];
 
     for (let i = 0, l = args.length; i < l; i+=2) {
         const prop = args[i].replace('--', '');
         const val = args[i + 1];
-        console.log(`${prop} is ${val}`);
+        result.push(`${prop} is ${val}`);
     }
-
+    console.log(result.join(', '));
 };
 
 parseArgs();
