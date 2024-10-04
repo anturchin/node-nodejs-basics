1f63c226dcfa53769af65ed3af318b1ecebf5c7f
tar <tur4intwo@gmail.com>
2024-10-04 10:38:45 +0300

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+
+    const args = process.argv.slice(2);
+
+    for (let i = 0, l = args.length; i < l; i+=2) {
+        const prop = args[i].replace('--', '');
+        const val = args[i + 1];
+        console.log(`${prop} is ${val}`);
+    }
+
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,10 +1,13 @@
 const parseEnv = () => {
+
     const prefix = 'RSS_';
+
     const filteredVariables = Object.entries(process.env)
         .filter(([key]) => key.startsWith(prefix))
         .map(([key, value]) => `${key}=${value}`)
         .join('; ');
     console.log(filteredVariables);
+
 };
 
 parseEnv();
